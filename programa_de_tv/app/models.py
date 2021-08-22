340144df2de1bbcf990e0229c5e8ddbb77276988
Claudio <clauchile@gmail.com>
2021-08-22 17:59:44 -0400

    se agregó íconos y validaciones

--- programa_de_tv/app/models.py
+++ programa_de_tv/app/models.py
@@ -1,5 +1,21 @@
 from django.db import models
 
+class ShowManager(models.Manager):
+    def basic_validator(self, postData):
+        errors = {}
+        # agregue claves y valores al diccionario de errores para cada campo no válido
+        if len(postData['titulo']) < 2:
+            errors["largo_titulo"] = "El título debería tener al menos 2 carácteres"
+        if len(postData['plataforma']) < 3:
+            errors["largo_plataforma"] = "La plataforma debe tener al menos 3 caracteres"
+        if len(postData['descripcion']) >0:
+            if len(postData['descripcion'])<10 :
+        # if len((postData['descripcion'] >0) & len(postData['descripcion'] <10)):
+                errors["largo_descripcion"] = "La descripción debería tener al menos 10 caracteres"
+        if Show.objects.filter(title=postData['titulo']).exists():
+            errors['titulo_existe'] ='El titulo ya existe, favor ingresar otro'
+
+        return errors
 # Create your models here.
 class Show(models.Model):
     title= models.CharField(max_length= 250)
@@ -8,9 +24,12 @@ class Show(models.Model):
     description = models.CharField(max_length= 250)
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now_add = True)
+    objects= ShowManager() 
+
     def __str__(self):
         return f"{self.title} ({self.network})"
 
 
     def __repr__(self):
         return f"{self.title}"
+
--- programa_de_tv/app/views.py
+++ programa_de_tv/app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Show
+from django.contrib import messages
+
 
 def index(request):
     context = {
@@ -27,36 +29,91 @@ def new(request):
     if request.method == 'POST':
         print(request.POST)
 
-        peli = Show.objects.create(
-            title = request.POST['titulo'],
-            network = request.POST['plataforma'],
-            release_date = request.POST['release'],
-            description = request.POST['descripcion'],
-        )
-        
-        return redirect('/shows')    
+
+        if request.method == 'POST':
+        # print(request.POST)
+            errors = Show.objects.basic_validator(request.POST)
+        # compruebe si el diccionario de errores tiene algo en él
+        if len(errors) > 0:
+        #     # si el diccionario de errores contiene algo, recorra cada par clave-valor y cree un mensaje flash
+            for key, value in errors.items():
+                messages.error(request, value)
+                print(errors)
+                # request.session['show_titulo'] = request.POST['id']
+                request.session['show_titulo'] = request.POST['titulo']
+                request.session['show_plataforma'] =request.POST['plataforma']
+                request.session['show_release_date'] = request.POST['release_date']
+                request.session['show_descripcion'] =request.POST['descripcion']
+                
+                
+        #     # redirigir al usuario al formulario para corregir los errores
+            return redirect('/shows/new')
+
+        else:
+            
+            peli = Show.objects.create(
+                title = request.POST['titulo'],
+                network = request.POST['plataforma'],
+                release_date = request.POST['release_date'],
+                description = request.POST['descripcion'],
+            )
+            messages.add_message(request, 25 , f" el Show {request.POST['titulo']}, fue agregado con exito")
+            request.session['show_titulo'] = ""
+            request.session['show_plataforma'] = ""
+            request.session['show_release_date'] = ""
+            request.session['show_descripcion'] = ""
+            
+            return redirect('/shows')    
 
 def edit(request, dato):
 
     if request.method == 'GET':
-        print(dato)
+        # print(dato)
         context = {
         'editar' : Show.objects.get(id=dato) 
         }
+
+        data = Show.objects.get(id = dato)
+        print(f'{data.updated_at} + "aqui--->"') 
         return render(request, 'edit.html', context)
 
     if request.method == 'POST':
-        print(request.POST)
-        change = Show.objects.get(id=dato)
-        # print(change + "ok")
+        # print(request.POST)
+        errors = Show.objects.basic_validator(request.POST)
+        # compruebe si el diccionario de errores tiene algo en él
+        if len(errors) > 0:
+        #     # si el diccionario de errores contiene algo, recorra cada par clave-valor y cree un mensaje flash
+            for key, value in errors.items():
+                messages.error(request, value)
+                print(errors)
+                # request.session['show_titulo'] = request.POST['id']
+                request.session['show_titulo'] = request.POST['titulo']
+                request.session['show_plataforma'] =request.POST['plataforma']
+                request.session['show_release_date'] = request.POST['release_date']
+                request.session['show_descripcion'] =request.POST['descripcion']
+                
+                
+        #     # redirigir al usuario al formulario para corregir los errores
+            return redirect(f'/shows/{dato}/edit')
 
-        change.title = request.POST['titulo']
-        change.network = request.POST['plataforma']
-        change.release_date = request.POST['release_date']
-        change.description = request.POST['descripcion']
+        else:
+            change = Show.objects.get(id=dato)
+        #     # print(change + "ok")
 
-        change.save()
-        return redirect(f"/shows/{dato}")
+            change.title = request.POST['titulo']
+            change.network = request.POST['plataforma']
+            change.release_date = request.POST['release_date']
+            change.description = request.POST['descripcion']
+
+            change.save()
+            messages.add_message(request, 25 , f" el Show {dato}, fue actualizado con exito")
+            request.session['show_titulo'] = ""
+            request.session['show_plataforma'] = ""
+            request.session['show_release_date'] = ""
+            request.session['show_descripcion'] = ""
+
+
+            return redirect(f"/shows/{dato}")
 
     #     		○ c = ClassName.objects.get(id=1)
     # c.field_name = "algún valor nuevo para field_name"
@@ -82,6 +139,8 @@ def eliminar(request, dato):
     if request.method == 'GET':
         borrar = Show.objects.get(id=dato)
         borrar.delete()
+        messages.add_message(request, 20 , f" el Show {borrar.title}, fue eliminado con exito")
+
         # borrar.save()
     return redirect(f"/shows") 
     # if request.method == 'GET':
@@ -91,4 +150,23 @@ def eliminar(request, dato):
     #     }
     #     return render(request, 'edit.html', context)
 
-
+# def update(request, id):
+#     # pasar los datos al método que escribimos y guardar la respuesta en una variable llamada errores
+#     errors = Show.objects.basic_validator(request.POST)
+#         # compruebe si el diccionario de errores tiene algo en él
+#     if len(errors) > 0:
+#         # si el diccionario de errores contiene algo, recorra cada par clave-valor y cree un mensaje flash
+#         for key, value in errors.items():
+#             messages.error(request, value)
+#         # redirigir al usuario al formulario para corregir los errores
+#         return redirect(f'/shows/+{id}')
+#     else:
+#         # si el objeto de errores está vacío, eso significa que no hubo errores.
+#         # recuperar el blog para actualizarlo, realizar los cambios y guardar
+#         show = Show.objects.get(id = id)
+#         show.desc = request.POST['desc']
+#         show.save()
+#         show.name = request.POST['nombre']
+#         messages.success(request, "Blog successfully updated")
+#         # redirigir a la ruta de exito
+#         return redirect('/shows')
